a039774cf9dc1f60bc2ccffe367e2e7361752976
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-07-07 23:52:32 +0530

    [Shubham][7-July] Code change

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -8,6 +8,7 @@ function HomePage(){
         <div className='home'>
             <div className='home_container'>
                 <img src='https://images.pexels.com/photos/1050244/pexels-photo-1050244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' className='home_img'/>
+                <div className='home_row_container'>
                 <div className='home_row'>
                     <Product/>
                     <Product/>
@@ -21,6 +22,7 @@ function HomePage(){
                     <Product/>
                     <Product/>
                     <Product/>
+                </div>
                 </div>
             </div>
         </div>
