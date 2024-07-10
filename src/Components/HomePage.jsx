87db0afeae3cdc7b07b0fbdd31ca360a242695e2
ssinha8752 <shubhamsinha8752@yahoo.com>
2024-07-11 00:27:33 +0530

    [Shubham][11-July] Code change

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Product from './Product'
 import './HomePage.css'
+import productsData from './data.json';
 
 function HomePage(){
   return (
@@ -9,20 +10,16 @@ function HomePage(){
             <div className='home_container'>
                 <img src='https://images.pexels.com/photos/1050244/pexels-photo-1050244.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='' className='home_img'/>
                 <div className='home_row_container'>
-                <div className='home_row'>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                </div>
-                <div className='home_row'>
-                    <Product/>
-                    <Product/>
-                </div>
-                <div className='home_row'>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                </div>
+                    <div className='home_row'>
+                    {productsData.products.map((product, index) => (
+                    <Product 
+                        key={index} 
+                        item_name={product.item_name} 
+                        price={product.price} 
+                        source_image={product.source_image} 
+                    />
+                    ))}
+                    </div>
                 </div>
             </div>
         </div>
